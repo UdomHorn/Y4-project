9bafd7f4e44a68669221d3ceff097f0104675408
udom04 <udomhorn04@gmail.com>
2024-12-17 15:18:34 +0700

    first commit

---
+++ display.php
@@ -0,0 +1,12 @@
+<?php
+$n=$_POST['tfn'];
+$gen=$_POST['gender'];
+$g=$_POST['tfg'];
+$y=$_POST['tfy'];
+echo "Name=".$n."<br/>";
+echo "Gender=".$gen."<br/>";
+echo "Group=".$g."<br/>";
+echo "Year=".$y."<br/>";
+?>
+<br>
+<a href="studentform2.html">Back</a>
---
+++ myscript.js
@@ -0,0 +1,4 @@
+var d=new Date();
+function getRandom(min,max){
+    return min+Math.floor(Math.random()*(max-min+1));
+}
